2a5b4466a1af54dc6657a9ebe9e74f0cae33bc1b
thedole <thedole@gmail.com>
2025-10-16 03:24:45 +0200

    Bookbot takes path to book from command line argument and prints report

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import sys
 from stats import count_words
+from stats import count_characters
+from stats import sort_character_dictionary
 
 def get_book_text(filepath):
     if filepath == None or filepath == "":
@@ -11,8 +14,27 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
-    frankenstein = get_book_text("books/frankenstein.txt")
-    num_words = count_words(frankenstein)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    path = sys.argv[1]
+
+    book = get_book_text(path)
+    num_words = count_words(book)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
 
+    print("--------- Character Count -------")
+    character_count = count_characters(book)
+    character_list = sort_character_dictionary(character_count)
+    for character_dict in character_list:
+        if character_dict["char"].isalpha():
+            print(f"{character_dict["char"]}: {character_dict["num"]}")
+    
+    print("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,26 @@
 def count_words(string):
     words = string.split()
     return len(words)
+
+def count_characters(string):
+    lowered_string = string.lower()
+    dict = {}
+
+    for char in lowered_string:
+        if char not in dict:
+            dict[char] = 0
+        dict[char] += 1
+    
+    return dict
+
+def sort_character_dictionary(characters):
+    character_list = []
+
+    for character, count in characters.items():
+        character_list.append({"char": character, "num": count})
+
+    sort_on = lambda item: item["num"]
+
+    character_list.sort(key=sort_on, reverse=True)
+
+    return character_list
